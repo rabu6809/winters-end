2af347a1a5f55f2c0df853b09dcb74568556191c
rabu6809 <133813254+rabu6809@users.noreply.github.com>
2024-02-06 07:44:35 -0600

    Add  human class

--- game.py
+++ game.py
@@ -2,3 +2,18 @@
 from random import *
 from math import *
 #------------------------------------
+# battle system
+class human:
+  def __init__(perk):
+    self.hp = 0
+    self.bleed = 0
+    self.ableed = 0
+    self.cripple = 0
+    self.fracture = 0
+    self.i4s = 0
+    self.perk = perk
+    self.perkcd = 0
+    self.ammo = 0
+    self.gundmg = 0
+    self.meleedmgh = 10
+    self.meleedmgl = 5
